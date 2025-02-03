e0428eb82fd1a922e6106a49e5daf87f103f76d4
Ismael Velazquez <128411935+Ivela183@users.noreply.github.com>
2025-02-03 01:31:24 +0000

    First Commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
+public class BankAccount{
+    public static void main(String[] arg){
+        float balance = 2175.37F; //Starting balance
+        balance -= 302.50;//Withdrew 302.50
+        balance += 50.03;//Deposited 50.03
+        balance /= 2;//Withdrew half of his balance
+        balance += 20;//Deposited 20
+        balance--;//Withdrew 1
+        balance *= 2;//Deposited the same amount which double his amount
+        balance++;//Depodited 1
+        System.out.println(balance);// New Balance
 
+
+    }
+}
